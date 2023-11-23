5628141539d332cc1ebc5d71370310065d4b2c46
ErgothDK <ergothdk@gmail.com>
2023-11-23 13:56:51 -0600

    Added meetups components and starting page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,25 @@
+import MeetupList from "@/components/meetups/MeetupList";
 import Image from "next/image";
 
-export default function Home() {
-  return <h1>Hello Home Page!</h1>;
+export default function HomePage() {
+  const DUMMY_EVENTS = [
+    {
+      id: Math.random().toString(),
+      title: "Brunch Meetings",
+      description: "Meet new people while enjoy a brunch",
+      image:
+        "https://www.meetup.com/blog/wp-content/uploads/2022/03/friends-hiking_turn-casual-friendships-into-close-friendships-1120x630.jpg",
+      address: "C. 3 Sur s/n · Playa del Carmen, Qro",
+    },
+    {
+      id: Math.random().toString(),
+      title: "Birthday Meeting",
+      description: "A meeting to celebrate multiple people birthdays",
+      image:
+        "https://secure.meetupstatic.com/photos/event/6/2/c/5/600_517165285.webp?w=384",
+      address: "Calle 44 Nte · Playa del Carmen, Qro",
+    },
+  ];
+
+  return <MeetupList meetups={DUMMY_EVENTS} />;
 }
